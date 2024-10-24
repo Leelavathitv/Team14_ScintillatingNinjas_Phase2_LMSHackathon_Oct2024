fa641acc01f568d719b7fe60fa2179041df45ba0
RathnaSridhar <rathnasridhar@gmail.com>
2024-10-24 23:29:38 +0530

    completed framework and added login & dashboard feature

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,6 +2,7 @@ package runner;
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
@@ -10,11 +11,11 @@ import io.cucumber.testng.CucumberOptions;
 
 
 	@CucumberOptions(
-			 features = {"./src/test/resources/features"},
-	    glue = {"stepdefinitions","hooks"},
+			 features = {"src/test/resources/feature"},
+	    glue = {"stepDefinition","hooks"},
 	    monochrome = true,
 
-	   tags= "@Login",
+	   tags= "",
 	    plugin= {"pretty","html:target/cucumber-reports/cucumber.html",
 	    		//"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 	    		// "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
@@ -25,7 +26,7 @@ import io.cucumber.testng.CucumberOptions;
 		
 		@BeforeTest
 		@Parameters({"browser"})
-		public void defineBrowser(String browser) {
+		public void defineBrowser(@Optional ("chrome") String browser) {
 			//ConfigFileReader.setBrowserType(browser);
 		}
 		@Override
--- src/test/java/testContext/TestContext.java
+++ src/test/java/testContext/TestContext.java
@@ -1,5 +1,36 @@
 package testContext;
 
+import org.openqa.selenium.WebDriver;
+
+import driverManager.DriverManager;
+
+import managers.PageObjectManager;
+import utilities.ResourceBundleReader;
+
 public class TestContext {
+	
+	WebDriver driver;
+	DriverManager driverManager;
+	PageObjectManager pageObjectManager;
+	ResourceBundleReader resourceBundleReader;
+	
+	public TestContext() {
+		driverManager =  new DriverManager();
+		pageObjectManager = new PageObjectManager(DriverManager.getDriver());
+		resourceBundleReader = new ResourceBundleReader();
+	}
+	
+	public DriverManager getDriverManager() {
+		return driverManager;
+		
+	}
+	
+	public ResourceBundleReader getResourceBundleReader() {
+		return resourceBundleReader;
+	}
+	
+	public PageObjectManager getPageObjectManager() {
+		return pageObjectManager;
+	}
 
 }
