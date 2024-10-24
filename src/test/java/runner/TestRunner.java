d374a36b146f9ddc05d6fbab75958b749cf3a7eb
RathnaSridhar <rathnasridhar@gmail.com>
2024-10-25 00:20:11 +0530

    added features and Stepdefs

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,6 +2,7 @@ package runner;
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
@@ -9,31 +10,30 @@ import io.cucumber.testng.CucumberOptions;
 
 
 
-	@CucumberOptions(
-			 features = {"./src/test/resources/features"},
-	    glue = {"stepDefinitions","hooks"},
-	    monochrome = true,
+@CucumberOptions(
+		 features = {"src/test/resources/feature"},
+   glue = {"stepDefinition","hooks"},
+   monochrome = true,
 
-	   tags= "@Login",
-	    plugin= {"pretty","html:target/cucumber-reports/cucumber.html",
-	    		//"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-	    		// "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-	    		// "rerun:target/rerun.txt",
-	    		})
+  tags= "",
+   plugin= {"pretty","html:target/cucumber-reports/cucumber.html",
+   		//"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+   		// "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+   		// "rerun:target/rerun.txt",
+   		})
 
-	 public class TestRunner extends AbstractTestNGCucumberTests{
-		
-		@BeforeTest
-		@Parameters({"browser"})
-		public void defineBrowser(String browser) {
-			//ConfigFileReader.setBrowserType(browser);
-		}
-		@Override
-	    @DataProvider(parallel = false)//true means execute parallely false mean not execute parallely
-		
-	    public Object[][] scenarios() {
-					
-			return super.scenarios();
-	    }	
-
+public class TestRunner extends AbstractTestNGCucumberTests{
+	
+	@BeforeTest
+	@Parameters({"browser"})
+	public void defineBrowser(@Optional ("chrome") String browser) {
+		//ConfigFileReader.setBrowserType(browser);
+	}
+	@Override
+   @DataProvider(parallel = false)//true means execute parallely false mean not execute parallely
+	
+   public Object[][] scenarios() {
+				
+		return super.scenarios();
+   }	
 }
---
+++ src/test/java/stepDefinition/LoginStep.java
@@ -0,0 +1,221 @@
+package stepDefinition;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import pageObject.DashboardPage;
+import testContext.TestContext;
+import utilities.ResourceBundleReader;
+
+public class LoginStep {
+	
+	TestContext testContext;
+	ResourceBundleReader resourceBundleReader;
+	WebDriver driver;
+	DashboardPage dashboard;
+	Logger logger= LogManager.getLogger(LoginStep.class);
+	
+	@SuppressWarnings("static-access")
+	public LoginStep(TestContext testcontext) {
+		this.testContext = testcontext;
+		this.dashboard = testcontext.getPageObjectManager().getDashboardPage();
+		this.resourceBundleReader = testcontext.getResourceBundleReader();
+		this.driver = testcontext.getDriverManager().getDriver();
+	}
+	
+	@Given("Admin launch the browser")
+	public void admin_launch_the_browser() {
+//	System.out.println("Driver value in stepdef *******" +driver);
+		logger.info("Admin launched the Browser");
+	   
+	}
+
+	@When("Admin gives the correct LMS portal URL")
+	public void admin_gives_the_correct_lms_portal_url() {
+		
+	}
+
+	@Then("Admin should land on the login page")
+	public void admin_should_land_on_the_login_page() {
+	    
+	}
+
+	@When("Admin gives the invalid LMS portal URL")
+	public void admin_gives_the_invalid_lms_portal_url() {
+	    
+	}
+
+	@Then("Admin should recieve {int} page not found error")
+	public void admin_should_recieve_page_not_found_error(Integer int1) {
+	    
+	}
+
+	@Then("HTTP response >= {int} and the link is broken")
+	public void http_response_and_the_link_is_broken(Integer int1) {
+
+	}
+
+	@Given("Admin launches the browser")
+	public void admin_launches_the_browser() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should receive {int} page not found error")
+	public void admin_should_receive_page_not_found_error(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("HTTP response >= {int}. Then the link is broken")
+	public void http_response_then_the_link_is_broken(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see correct spellings in all fields")
+	public void admin_should_see_correct_spellings_in_all_fields() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see the logo on the left side")
+	public void admin_should_see_the_logo_on_the_left_side() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see {string}")
+	public void admin_should_see(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see the company name below the app name")
+	public void admin_should_see_the_company_name_below_the_app_name() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see two text fields")
+	public void admin_should_see_two_text_fields() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see {string} in the first text field")
+	public void admin_should_see_in_the_first_text_field(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see field mandatory aestrik symbol next to Admin text")
+	public void admin_should_see__field_mandatory_aestrik_symbol_next_to_admin_text() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see {string} in the second text field")
+	public void admin_should_see_in_the_second_text_field(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see aestrik symbol next to the password text")
+	public void admin_should_see_aestrik_symbol_next_to_the_password_text() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see the text fields aligned correctly")
+	public void admin_should_see_the_text_fields_aligned_correctly() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see login button")
+	public void admin_should_see_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see login button on the centre of the page")
+	public void admin_should_see_login_button_on_the_centre_of_the_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see Admin in gray color")
+	public void admin_should_see_admin_in_gray_color() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should see password in gray color")
+	public void admin_should_see_password_in_gray_color() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Admin enter valid credentials  and clicks login button")
+	public void admin_enter_valid_credentials_and_clicks_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should land on dashboard page \\( centre of the page will be empty , menu bar is present).")
+	public void admin_should_land_on_dashboard_page_centre_of_the_page_will_be_empty_menu_bar_is_present() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Admin enter invalid credentials  and clicks login button")
+	public void admin_enter_invalid_credentials_and_clicks_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Error message please check Adminname\\/password")
+	public void error_message_please_check_adminname_password() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Admin enter value only in password and clicks login button")
+	public void admin_enter_value_only_in_password_and_clicks_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Admin enter value only in Adminname and clicks login button")
+	public void admin_enter_value_only_in_adminname_and_clicks_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Admin enter valid credentials  and clicks login button through keyboard")
+	public void admin_enter_valid_credentials_and_clicks_login_button_through_keyboard() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Admin should land on dashboard page")
+	public void admin_should_land_on_dashboard_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Admin enter valid credentials  and clicks login button through mouse")
+	public void admin_enter_valid_credentials_and_clicks_login_button_through_mouse() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
+}
