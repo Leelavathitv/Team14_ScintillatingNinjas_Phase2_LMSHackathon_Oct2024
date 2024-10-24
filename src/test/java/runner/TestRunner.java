bf6c439c143eb80df990857cc62b71fb4f9cb2d5
RathnaSridhar <rathnasridhar@gmail.com>
2024-10-24 23:45:15 +0530

    "Merging Rathna and origin"

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,7 +2,6 @@ package runner;
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
@@ -11,11 +10,11 @@ import io.cucumber.testng.CucumberOptions;
 
 
 	@CucumberOptions(
-			 features = {"src/test/resources/feature"},
-	    glue = {"stepDefinition","hooks"},
+			 features = {"./src/test/resources/features"},
+	    glue = {"stepDefinitions","hooks"},
 	    monochrome = true,
 
-	   tags= "",
+	   tags= "@Login",
 	    plugin= {"pretty","html:target/cucumber-reports/cucumber.html",
 	    		//"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
 	    		// "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
@@ -26,7 +25,7 @@ import io.cucumber.testng.CucumberOptions;
 		
 		@BeforeTest
 		@Parameters({"browser"})
-		public void defineBrowser(@Optional ("chrome") String browser) {
+		public void defineBrowser(String browser) {
 			//ConfigFileReader.setBrowserType(browser);
 		}
 		@Override
