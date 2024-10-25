a865e7a2723d66fe0c8683e3192967340652537b
Saritha KS <sarithaguptaks@gmail.com>
2024-10-24 22:56:58 -0500

    committing program features and step definitions

---
+++ src/test/java/stepDefinitions/ProgramStep.java
@@ -0,0 +1,223 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class ProgramStep {
+	@Given("Admin is logged in to LMS Portal")
+	public void admin_is_logged_in_to_lms_portal() {
+	   
+	}
+
+	@Given("Admin is on dashboard page after Login")
+	public void admin_is_on_dashboard_page_after_login() {
+	   
+	}
+
+	@When("Admin clicks {string} on the navigation bar")
+	public void admin_clicks_on_the_navigation_bar(String string) {
+	   
+	}
+
+	@Then("Admin should be navigated to Program module")
+	public void admin_should_be_navigated_to_program_module() {
+	   
+	}
+
+	@Then("Admin should not have any broken links for Program module")
+	public void admin_should_not_have_any_broken_links_for_program_module() {
+	   
+	}
+
+	@Then("Admin should see the heading {string}")
+	public void admin_should_see_the_heading(String string) {
+	    
+	}
+
+	@Then("Admin should see the module names as in order {string}")
+	public void admin_should_see_the_module_names_as_in_order(String string) {
+	   
+	}
+
+	@Then("Admin should see Logout in menu bar")
+	public void admin_should_see_logout_in_menu_bar() {
+	    
+	}
+
+	@Given("Admin is on program page")
+	public void admin_is_on_program_page() {
+	    	}
+
+	@Then("Admin should see sub menu in menu bar as {string}")
+	public void admin_should_see_sub_menu_in_menu_bar_as(String string) {
+	   
+	}
+
+	@Then("Admin should able to see Program name, description, and status for each program")
+	public void admin_should_able_to_see_program_name_description_and_status_for_each_program() {
+	  
+	}
+
+	@Then("Admin should see a Delete button in left top is disabled")
+	public void admin_should_see_a_delete_button_in_left_top_is_disabled() {
+	   
+	}
+
+	@Then("Admin should see Search bar with text as {string}")
+	public void admin_should_see_search_bar_with_text_as(String string) {
+	    
+	}
+
+	@Then("Admin should see data table with column header on the Manage Program Page as  Program Name, Program Description, Program Status, Edit\\/Delete")
+	public void admin_should_see_data_table_with_column_header_on_the_manage_program_page_as_program_name_program_description_program_status_edit_delete() {
+	   
+	}
+
+	@Then("Admin should see checkbox default state as unchecked beside Program Name column header as")
+	public void admin_should_see_checkbox_default_state_as_unchecked_beside_program_name_column_header_as() {
+	    
+	}
+
+	@Then("Admin should see check box default state as unchecked on the left side in all rows against program name")
+	public void admin_should_see_check_box_default_state_as_unchecked_on_the_left_side_in_all_rows_against_program_name() {
+	   
+	}
+
+	@Then("Admin should see the sort arrow icon beside to each column header except Edit and Delete")
+	public void admin_should_see_the_sort_arrow_icon_beside_to_each_column_header_except_edit_and_delete() {
+	   
+	}
+
+	@Then("Admin should see the Edit and Delete buttons on each row of the data table")
+	public void admin_should_see_the_edit_and_delete_buttons_on_each_row_of_the_data_table() {
+	   
+	}
+
+	@Then("{string}{string}\" along with Pagination icon below the table.")
+	public void along_with_pagination_icon_below_the_table(String string, String string2) {
+	    
+	}
+
+	@Then("{string}{string}\".")
+	public void string_string(String string, String string2) {
+	   
+	}
+	
+	@Given("Admin is on program module after reaching dashboard")
+	public void admin_is_on_program_module_after_reaching_dashboard() {
+	   
+	}
+
+	@Given("Admin is on Program module")
+	public void admin_is_on_program_module() {
+	    
+	}
+
+	@When("Admin clicks on {string} under the {string} menu bar")
+	public void admin_clicks_on_under_the_menu_bar(String string, String string2) {
+	   
+	}
+
+	@Then("Admin should see pop up window for program details")
+	public void admin_should_see_pop_up_window_for_program_details() {
+	    
+	}
+
+	@Then("Admin should see window title as {string}")
+	public void admin_should_see_window_title_as(String string) {
+	    
+	}
+
+	@Then("Admin should see red {string} mark beside mandatory field {string}")
+	public void admin_should_see_red_mark_beside_mandatory_field(String string, String string2) {
+	    
+	}
+
+	@Given("Admin is on Program details form")
+	public void admin_is_on_program_details_form() {
+	    
+	}
+
+	@When("Admin clicks save button without entering mandatory")
+	public void admin_clicks_save_button_without_entering_mandatory() {
+	    
+	}
+
+	@Then("Admin gets message {string}")
+	public void admin_gets_message(String string) {
+	    
+	}
+
+	@When("Admin clicks Cancel button")
+	public void admin_clicks_cancel_button() {
+	    
+	}
+
+	@Then("Admin can see Program Details form disappears")
+	public void admin_can_see_program_details_form_disappears() {
+	    
+	}
+
+	
+
+	@When("Admin enters the Name in the text box")
+	public void admin_enters_the_name_in_the_text_box() {
+	    
+	}
+
+	@Then("Admin can see the text entered")
+	public void admin_can_see_the_text_entered() {
+	    
+	}
+
+	@When("Admin enters the Description in text box")
+	public void admin_enters_the_description_in_text_box() {
+	    
+	}
+
+	@Then("Admin can see the text entered in description box")
+	public void admin_can_see_the_text_entered_in_description_box() {
+	   
+	}
+
+	@When("Admin selects the status of the program by clicking on the radio button {string}")
+	public void admin_selects_the_status_of_the_program_by_clicking_on_the_radio_button(String string) {
+	    
+	}
+
+	@Then("Admin can see {string} status selected")
+	public void admin_can_see_status_selected(String string) {
+	    
+	}
+
+	@When("Admin enter valid details for mandatory fields and Click on save button")
+	public void admin_enter_valid_details_for_mandatory_fields_and_click_on_save_button() {
+	    
+	}
+
+	@When("Admin Click on cancel button")
+	public void admin_click_on_cancel_button() {
+	    
+	}
+
+	@Then("Admin can see program details form disappear")
+	public void admin_can_see_program_details_form_disappear() {
+	  
+	}
+
+	@When("Admin searches with newly created {string}")
+	public void admin_searches_with_newly_created(String string) {
+	   
+	}
+
+	@Then("Records of the newly created {string} is displayed and match the data entered")
+	public void records_of_the_newly_created_is_displayed_and_match_the_data_entered(String string) {
+	    
+	}
+
+	@When("Admin Click on {string} button")
+	public void admin_click_on_button(String string) {
+	    
+	}
+
+
+}
